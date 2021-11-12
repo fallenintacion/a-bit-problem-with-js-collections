9bced01d24579e514b2f43ef3b9220f46f030cbf
fallenintacion <77117418+fallenintacion@users.noreply.github.com>
2021-11-12 23:54:57 +0300

    Add files via upload

---
+++ collections_and_smt/script.js
@@ -0,0 +1,9 @@
+let radioButtons = document.querySelectorAll('.radiobutton');
+let submitButton = document.querySelector('.submit-button');
+
+radioButtons.onchange = function(){
+    console.log('+');
+
+
+
+}
