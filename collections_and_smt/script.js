ad5c1d289302d744dbb02002bd00299b07f70149
fallenintacion <77117418+fallenintacion@users.noreply.github.com>
2021-11-13 00:05:07 +0300

    Update script.js

--- collections_and_smt/script.js
+++ collections_and_smt/script.js
@@ -1,9 +1,21 @@
 let radioButtons = document.querySelectorAll('.radiobutton');
 let submitButton = document.querySelector('.submit-button');
 
-radioButtons.onchange = function(){
-    console.log('+');
+for (rButton of radioButtons){
+    rButton.onchange = function () {
+        console.log(rButton.dataset.evaluation);
+
+        /*
+        if (rButton.dataset.evaluation === 'bad'){
+            submitButton.disabled = true;
+        }
+        else{
+            submitButton.disabled = false;
+
+        }
+        */
 
 
+    }
 
 }
